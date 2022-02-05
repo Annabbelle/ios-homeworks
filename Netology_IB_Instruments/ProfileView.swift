19438c15fe7ccd1f2444f7d61f6249626ef3efdc
Annabbelle <chupirchuksolomiya@gmail.com>
2022-02-05 19:27:53 +0100

    добавила нужные файлы

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by MacBook on 05.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var ImagePro: UIImageView!
+    
+    @IBOutlet weak var NamePro: UILabel!
+    
+    @IBOutlet weak var BirthdayPro: UILabel!
+    
+    @IBOutlet weak var TownPro: UILabel!
+    
+    
+    @IBOutlet weak var TextPro: UITextView!
+    
+}
